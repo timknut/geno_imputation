5c59c03291524f439f1227bbf96c5389ea1964d4
Tim Knutsen <timknutsen@gmail.com>
2016-07-13 11:44:55 +0200

    GenomeList_2_plink.sh now prints simple usage instructions

--- scripts/GenomeList_2_plink.sh
+++ scripts/GenomeList_2_plink.sh
@@ -3,16 +3,34 @@
 # Tim Knutsen
 # Mon Jul 11 21:21:08 CEST 2016
 
-# Script takes an illumina Finalreport in GenomeList format and outputs 
-# a plink *.ped file automatically named infile.ped. Convertion is done by calling
-# snptranslate present in this repo.
+# Add simple usage instructions. 
+display_usage() {
+	echo
+	echo -e "Script takes an illumina Finalreport in GenomeList format and outputs"
+	echo "a plink *.ped file automatically named [infile].ped. Convertion is done by calling"
+	echo "snptranslate present in this repo." 
+	echo -e "\nUsage: sh $0 [FinalReport file] \n"
+}	 
 
-# Usage: sh GenomeList_2_plink.sh [FinalReport file]
+# if less than two arguments supplied, display usage 
+if [  $# -le 1 ]
+	then
+	display_usage
+	exit 1
+fi 
 
-prefix=/mnt/users/tikn/for_folk/geno # Guess we need to have two prefixes and uncomment based on who is using it. Tim  
-prefix= /Users/paolo/Documents/Roslin/Geno_project/git_imputation/ # Guess we need to have two prefixes and uncomment based on who is using it. Paolo 
+# check whether user had supplied -h or --help . If yes display usage 
+if [[ ( $# == "--help") ||  $# == "-h" ]]
+	then 
+	display_usage
+	exit 0
+fi 
+
+prefix=/mnt/users/tikn/for_folk/geno # Tim  
+#prefix=/Users/paolo/Documents/Roslin/Geno_project/git_imputation/ # Paolo 
 
 infile=$1
+
 outfile=$(basename $1 .txt).ped 
 ioSNP=${prefix}/geno_imputation/scripts/snptranslate/ioSNP.py
 
