54ff920ab244fd58d0115fbb7a7ec009fac43554
Tim Knutsen <timknutsen@gmail.com>
2016-07-13 23:51:43 +0200

    Convertion script takes .map file as argument.

--- scripts/Genome_2_plink.sh
+++ scripts/Genome_2_plink.sh
@@ -3,17 +3,20 @@
 # Tim Knutsen
 # Mon Jul 11 21:21:08 CEST 2016
 
+set -o nounset   # Prevent unset variables from been used.
+set -o errexit   # Exit if error occurs
+
 # Add simple usage instructions. 
 display_usage() {
 	echo
 	echo -e "Script takes an illumina Finalreport in GenomeList format and outputs"
 	echo "a plink *.ped file automatically named plink_format/[infile].ped. Convertion is done by calling"
 	echo "snptranslate present in this repo." 
-	echo -e "\nUsage: sh $0 [FinalReport file] \n"
+	echo -e "\nUsage: sh $0 [FinalReport file] [input-format](Genomelist or Genomematrix) [markerfile] \n"
 }	 
 
-# if less than two arguments supplied, display usage 
-if [  $# -eq 0 ]
+# if less than 3 arguments supplied, display usage 
+if [  $# -le 2 ]
 	then
 	display_usage
 	exit 1
@@ -31,12 +34,15 @@ prefix=/mnt/users/tikn/for_folk/geno # Tim
 ioSNP=${prefix}/geno_imputation/scripts/snptranslate/ioSNP.py
 
 infile=$1
+informat=$2
 infiledir=$(dirname "$infile")
 mkdir -p ${infiledir}/plink_format
 outfile=${infiledir}/plink_format/$(basename $1 .txt).ped 
 outfile2=${infiledir}/plink_format/$(basename $1 .txt).map
-markerfile=${prefix}/geno_imputation/genotype_rawdata/marker_mapfiles/illumina54k_v2_annotationfile.map
-# Test outfile
-echo "Writing $outfile and $outfile2" 
+#markerfile=${prefix}/geno_imputation/genotype_rawdata/marker_mapfiles/illumina54k_v2_annotationfile.map
+markerfile=$3
 
-"$ioSNP" -i "$infile" -n Genomelist -o "$outfile" -u Plink --output2 $outfile2 -m $markerfile
+# Test outfile
+echo "Writing $outfile and $outfile2. Using $markerfile" 
+
+"$ioSNP" -i "$infile" -n $informat -o "$outfile" -u Plink --output2 $outfile2 -m $markerfile
--- scripts/split_collectionfiles.sh
+++ scripts/split_collectionfiles.sh
@@ -3,8 +3,7 @@
 # Tim Knutsem
 # Wed Jul 13 22:10:55 CEST 2016
 
-## The raw data contained two concat. Genommatrix files. This script documents how they where split on the [Header] pattern.
-
+## The raw data contained two concat. Genomematrix files. This script documents how they where split on the [Header] pattern.
 
 # *_collection2
 awk '/^\[Header\]/{
@@ -12,7 +11,7 @@ awk '/^\[Header\]/{
 	} {
 	print > x;}' ../genotype_rawdata/illumina54k_v2/FinalReport_54kV2_collection2.txt
 
-# collection
+# collection_ed1
 awk '/^\[Header\]/{
 	x="../genotype_rawdata/illumina54k_v2/collection_Genomematrix_files/FinalReport_54kV2_collection_ed1_"++i".txt";
 	} {
